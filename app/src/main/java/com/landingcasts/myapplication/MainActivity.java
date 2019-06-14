b3f85339a3f7c4f5b422e4956b27950d60e21b86
Ogi G Permana <hunterdumay@gmail.com>
2019-06-14 19:25:13 +0700

    Implementasi Eksplisit Intent Dengan dan Tanpa Membawa Data

---
+++ app/src/main/java/com/landingcasts/myapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.landingcasts.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnMoveActivity;
+    Button btnMoveWithDataActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnMoveActivity = findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
+
+        btnMoveWithDataActivity = findViewById(R.id.btn_move_with_data_activity);
+        btnMoveWithDataActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_move_activity:
+                Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+                startActivity(moveIntent);
+                break;
+            case  R.id.btn_move_with_data_activity:
+                Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "DicodingAcademy Boy");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 5);
+                startActivity(moveWithDataIntent);
+                break;
+        }
+    }
+}
